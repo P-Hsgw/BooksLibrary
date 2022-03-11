e0452667f93d1395c8a678b254167f2c570be126
Patryk Hasegawa <contact@p-hsgw.com>
2022-03-11 11:55:19 +0900

    Initial Commit

---
+++ BooksLibrary/BooksLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksLibraryApp.swift
+//  BooksLibrary
+//
+//  Created by Patryk Hasegawa on 2022/03/11.
+//
+
+import SwiftUI
+
+@main
+struct BooksLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BooksLibrary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BooksLibrary
+//
+//  Created by Patryk Hasegawa on 2022/03/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
