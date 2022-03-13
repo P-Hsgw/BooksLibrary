540c0fd1daf461bec16acdf09da6b5b6bbceb997
Patryk Hasegawa <contact@p-hsgw.com>
2022-03-13 14:12:25 +0900

    Remove EditButton() - unused

--- BooksLibrary/ContentView.swift
+++ BooksLibrary/ContentView.swift
@@ -45,7 +45,6 @@ struct ContentView: View {
                         Image(systemName: "plus")
                             .foregroundColor(.orange)
                     }
-                    EditButton()
                 }
             }
             .navigationTitle("My Books")
